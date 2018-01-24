10a8e27616102173ba8938b67c65b200ed8dc3a7
mitchellangelo <mitchkeith.145@gmail.com>
2018-01-24 00:49:30 -0500

    loaf - updating the server-side unpack node script, which is a quick & dirty but functional way to automate the server side instructions for deploying the updated app.

--- src/unpack.js
+++ src/unpack.js
@@ -1,18 +1,34 @@
 const rimraf = require('rimraf');
 const fs = require('fs');
+const unzip = require('unzip');
 
 console.log('cleaning old directory... started');
 rimraf('./mitchkeith', (err) => {
 
-    if (typeof err === 'undefined') {
+    if (err === null || typeof err === 'undefined') {
         console.log('cleaning old directory... finished');
 
         console.log('creating new directory... started');
         fs.exists('./mitchkeith', (exists) => {
             if (!exists) {
                 fs.mkdir('./mitchkeith', (err) => {
-                    if (typeof err === 'undefined') {
+                    if (err === null || typeof err === 'undefined') {
                         console.log('creating new directory... finished');
+						console.log('archive copied... started');
+						fs.copyFile('/home/ec2-user/mitchkeith.zip', './mitchkeith/mitchkeith.zip', (err) => {
+							if (err === null || typeof err === 'undefined') {
+								console.log('archive copied... success');
+								console.log('unpacking archive... started');
+								fs.createReadStream('./mitchkeith/mitchkeith.zip').pipe(unzip.Extract({ path: './mitchkeith' })).on('close', (err) => {
+									if (err === null || typeof err === 'undefined') {								
+										console.log('unpacking archive... finished');
+									} else {
+										console.log('unpacking archive... finished, but in error');
+										console.log(err);
+									}
+								});
+							}
+						});
                     }
                 });
             } else {
@@ -22,7 +38,7 @@ rimraf('./mitchkeith', (err) => {
 
     } else {
         console.log('cleaning old directory... finished in error');
-        err.printStackTrace();
+		console.log(err);
     }
     
 
